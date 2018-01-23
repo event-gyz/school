05d120696c6c4220af7c18006ecef3ca0ea5baff
关雅卓 <yazhuo_guan@eventown.com>
2018-01-23 14:34:48 +0800

    1

--- inc/user_functions.php
+++ inc/user_functions.php
@@ -80,7 +80,7 @@ class MyUser
 		if(!$this->exist( $_EMAIL ))
 		{
 
-			$sql = "INSERT INTO member (id, password, first_name, last_name, cellphone, email) VALUES ('".$_EMAIL."',md5(lower('".$_PASS."')),'".$_FNAME."','".$_LNAME."','".$_PHONE."','".$_EMAIL."')";
+			$sql = "INSERT INTO member (`id`, password, first_name, last_name, cellphone, email) VALUES ('".$_EMAIL."',md5(lower('".$_PASS."')),'".$_FNAME."','".$_LNAME."','".$_PHONE."','".$_EMAIL."')";
 			$result = query($sql);
 
 			if($this->login($_EMAIL, $_PASS) == -1)
