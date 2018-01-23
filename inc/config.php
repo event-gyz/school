107c8bf6cefde17463c36a8a4a62331af419e240
关雅卓 <yazhuo_guan@eventown.com>
2018-01-23 13:51:29 +0800

    123

--- inc/config.php
+++ inc/config.php
@@ -12,16 +12,16 @@
 //    'DB_PORT' => '3306',
 //);
 
-$
-
-$config = array(db     = array(
+$db     = array(
     'DB_TYPE' => 'mysqli',
-    'DB_HOST' => '127.0.0.1',
+    'DB_HOST' => 'localhost',
     'DB_NAME' => 'school',
     'DB_USER' => 'root',
     'DB_PWD'  => 'root',
     'DB_PORT' => '3306',
 );
+
+$config = array(
     'debug'   => 1, //开启debug
     'log'     => 1, //开启日志记录
     'LOGFILE' => date('Ymd') . ".log", //日志存放的文件位置，默认当前项目目录下时间戳为单位
