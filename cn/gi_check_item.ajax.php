92425b98996a49c1d674de0db6580598e883350f
关雅卓 <yazhuo_guan@eventown.com>
2018-02-06 13:00:43 +0800

    1

--- cn/gi_check_item.ajax.php
+++ cn/gi_check_item.ajax.php
@@ -18,14 +18,14 @@ if(!empty($row)) {
 	$type = $row['type'];	
 	$ret = 0;	
 	if(!$is_learned) {
-		$is_late = ($user_age > $age_max);
+		$is_late = ($user_age > $age_max)?1:0;
 		$sql = "DELETE FROM grow_log WHERE user_uid='$user_uid' AND item_uid='$item_uid'";
         $result = M()->execute($sql);
 		$arr = array('uid'=>$item_uid,'is_late'=>$is_late);//,'age_max'=>$age_max,'user_age'=>$user_age);
 		die(genResponse($result, json_encode($arr)));
 	}
 	else {
-		$is_early = ($user_age < $age_min);
+        $is_early = ($user_age < $age_min)?1:0;
 		$sql = "INSERT INTO grow_log (user_uid, item_uid, early,type) values ('$user_uid','$item_uid','$is_early','$type') on duplicate key update item_uid=item_uid";
         $result = M()->execute($sql);
 		$arr = array('uid'=>$item_uid,'is_early'=>$is_early);
