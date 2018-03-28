c09fce4cf6866c0894306fbbd5912aade92d211e
关雅卓 <yazhuo_guan@eventown.com>
2018-03-28 16:32:47 +0800

    12

--- cn/getOpenId.php
+++ cn/getOpenId.php
@@ -17,27 +17,28 @@ if(!isset($_GET['code'])){
     $secret = "bbd2828138fbd24fc5747ec370572fc0";
     $code = $_GET["code"];
     $get_token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$secret.'&code='.$code.'&grant_type=authorization_code';
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$get_token_url);
-    curl_setopt($ch,CURLOPT_HEADER,0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-    $res = curl_exec($ch);
-    curl_close($ch);
+//    $ch = curl_init();
+//    curl_setopt($ch,CURLOPT_URL,$get_token_url);
+//    curl_setopt($ch,CURLOPT_HEADER,0);
+//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+//    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+//    $res = curl_exec($ch);
+//    curl_close($ch);
+    $res = file_get_contents($get_token_url);
     $json_obj = json_decode($res,true);
     //根据openid和access_token查询用户信息
     $access_token = $json_obj['access_token'];
     $openid = $json_obj['openid'];
-    $get_user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+    $get_user_info_url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
 
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$get_user_info_url);
-    curl_setopt($ch,CURLOPT_HEADER,0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-    $res = curl_exec($ch);
-    curl_close($ch);
-
+//    $ch = curl_init();
+//    curl_setopt($ch,CURLOPT_URL,$get_user_info_url);
+//    curl_setopt($ch,CURLOPT_HEADER,0);
+//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+//    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+//    $res = curl_exec($ch);
+//    curl_close($ch);
+    $res = file_get_contents($get_user_info_url);
     //解析json
     $user_obj = json_decode($res,true);
     $_SESSION['user'] = $user_obj;
