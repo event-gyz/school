e21926df55e8384d8be78153b8ac8497b9d4402b
关雅卓 <yazhuo_guan@eventown.com>
2018-03-29 11:57:38 +0800

    11

--- cn/inc_fancyboxes.php
+++ cn/inc_fancyboxes.php
@@ -1,7 +1,12 @@
 <?php
 include("fancybox/login.html");
+include("fancybox/mobile_bind.html");
+include("fancybox/mobile_register.html");
+include("fancybox/mobile_login.html");
 include("fancybox/register01.html");
 include("fancybox/register02.html");
+include("fancybox/complete_info01.html");
+include("fancybox/complete_info02.html");
 include("fancybox/forget.html");
 include("fancybox/modify_member.html");
 include("fancybox/modify_baby.html");
