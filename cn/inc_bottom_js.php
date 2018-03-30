80e2f3d8a38c83b795b839fc1e58bd0a01248039
关雅卓 <yazhuo_guan@eventown.com>
2018-03-30 15:31:13 +0800

    12

--- cn/inc_bottom_js.php
+++ cn/inc_bottom_js.php
@@ -462,8 +462,7 @@
         });
     }
 
-    function showLoginStatus() {
-        var nickname = <?=$_SESSION['user_email']?>;
+    function showLoginStatus(nickname,points) {
         $("#login_status").html('<li class="m_name-point fst"><b>'+nickname+'</b></li><li><a href="javascript:doLogout();">登出</a></li>');
         $("#login_status_m").html('<b>'+nickname+'</b><a href="javascript:doLogout();">登出</a>');
         $("#ex_target_text").text("立即使用");
