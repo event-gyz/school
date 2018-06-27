0d5f3ed45a26e3a9a564d56d9e1204bfacd36660
chenqintao <1620969243@qq.com>
2018-06-27 16:05:00 +0800

    添加心理课程页面

---
+++ cn/course.php
@@ -0,0 +1,114 @@
+<?php
+session_start();
+include('inc.php');
+?>
+
+<!DOCTYPE html>
+<html><!-- InstanceBegin template="/Templates/_page01.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+    <?php include('inc_head.php');	?>
+    <style>
+        body{background: #fff;}
+        h1,h2,h3,h4,h5,h6,p,ul,li,dl,dt,dd{margin:0;padding:0;list-style: none;}
+        input,button{padding: 0;margin:0;border:0;outline: none;}
+        img{vertical-align: bottom}
+    </style>
+</head>
+
+<body>
+<!-- InstanceBeginEditable name="wrap" -->
+<section id="wrap" class="inpage">
+    <!-- InstanceEndEditable -->
+
+    <!--【Header】-->
+    <?php include 'inc_header.php'; ?>
+    <!--【Header End】-->
+
+    <!--【Content】-->
+    <section id="content">
+        <!-- InstanceBeginEditable name="content" -->
+
+        <!--//主內容//-->
+        <section class="indexcont">
+            <section class="inbox noBoxShadowPage">
+                <section class="contbox clearfix course">
+
+                    <!--//主選單標題與路徑//-->
+                    <div class="breadcrumbs_logo">
+                        <h2 class="title">育儿天地</h2>
+                        <section class="gopath"><a href="index.php">首页</a> > <a href="recommend.php">育儿天地</a> > <a href="children_psychology.php">儿童心理</a> > 儿童心理课程</section>
+                    </div>
+                    <ul class="course_detail_list">
+                        <li data-index="1">
+                            <div class="course_deatil">
+                                <h4>【育儿讲堂】如何透过运动建立孩子的高情商？</h4>
+                                <p><img src="../content/epaper/images/psychological_course.jpg" alt=""></p>
+                                <span>孩子自控能力差！</span><br>
+                                <span>孩子以自我为中心！</span><br>
+                                <span>孩子容易发脾气！</span><br>
+                                <span>孩子爱抱怨不能承担责任！</span><br>
+                                <span>孩子受挫能力弱，遇事不能面对！</span><br>
+                                <span>……</span>
+                                <div class="course_txt">
+                                    <h3>---情商的高低决定了孩子的处事能力和社交能力，高情商是可以透过后天培养的。</h3>
+                                    <p>虽然生活中有很多的“熊孩子”，但是，仍不缺乏高情商的“暖宝宝”，他们深受身边人的喜爱。那么如何提高孩子的情商呢？</p>
+                                    <ul>
+                                        <li>---巴布豆特邀  哈佛大学儿童心理专家</li>
+                                        <li><b>---巴布豆妈妈学院  儿童发展中心主任</b></li>
+                                        <li>
+                                            <b>许婴宁老师</b>  带您深入认识
+                                            <br>
+                                            关于儿童情商的必修课！
+                                        </li>
+                                    </ul>
+                                    <div class="required_course">
+                                        <h3><img src="../content/epaper/images/required_course.jpg" alt=""></h3>
+                                        <h4>【育儿讲堂】开讲啦！<br>—儿童情商必修课《一》</h4>
+                                        <p>在未来，智商可能决定孩子的能力，而情商则决定孩子人生发展的路径。做为家长都必须了解建立儿童情商的方法！那么，本节课程我们将主要谈谈如何透过【运动】提高孩子的情商！</p>
+                                        <p><b>运动已经是这个时代的一个标志！</b>运动不仅能够强健孩子的身体，也可以强大孩子的内心。现在的家庭普遍重视孩子的智商培养（比如：报补习班、早教机构上课。艺术课等），智商高虽然是孩子成功的因素之一。但是，影响孩子一生的是孩子的性格。他的世界观、价值观、耐心、信心、毅力、情绪、情感这些品质是孩子未来路上的良师益友，拥有了这些品质，孩子们离成功和幸福就会更进一步。大家知道吗？这些品质也可以透过后天的运动获得。也就是说运动可以让孩子的情商变的更强大！</p>
+                                    </div>
+                                    <div class="course_arrange">
+                                        <h3>【课程安排】</h3>
+                                        <h4>如何透过运动建立孩子高情商!</h4>
+                                        <p>2018年5月27日 晚上7：00准时开始！</p>
+                                        <span>您最关心的育儿话题，我们都为您开讲，巴布豆陪你一起养娃！</span>
+                                        <p class="course_arrange_info"><img src="../content/epaper/images/course_arrange.jpg" alt=""></p>
+                                    </div>
+                                    <div class="cooperative_partner">
+                                        <h3>【本期主题合作伙伴】</h3>
+                                        <p class="shoes"><img src="../content/epaper/images/shoes.jpg" alt=""></p>
+                                        <h4>-- 巴布豆童鞋 --<br>让运动的小脚丫都开心起来！</h4>
+                                        <p class="small_pants"><img src="../content/epaper/images/small_pants.jpg" alt=""></p>
+                                        <h4>-- 巴布豆小裤 --<br>好腰不夹PP 运动更加给力！</h4>
+                                        <p class="small_pants"><img src="../content/epaper/images/socks.jpg" alt=""></p>
+                                        <h4>-- 巴布豆童袜 --<br>给运动宝贝更自在的呵护！</h4>
+                                        <p class="small_pants"><img src="../content/epaper/images/raincoat.jpg" alt=""></p>
+                                        <h4>-- 巴布豆雨衣 --<br>什么都阻止不了宝贝爱动的心！</h4>
+                                        <p class="small_pants"><img src="../content/epaper/images/push_bike.jpg" alt=""></p>
+                                        <h4>-- 巴布豆滑步车 --<br>安全全舒适玩转运动！</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                            
+                    <!--//主選單標題與路徑//-->
+
+                </section>
+            </section>
+        </section>
+        <!--//主內容//-->
+
+        <!-- InstanceEndEditable -->
+    </section>
+    <!--【Content End】-->
+
+    <!--【Footer】-->
+    <?php include 'inc_footer.html'; ?>
+    <!--【Footer End】-->
+
+
+</section>
+<?php include 'inc_bottom_js.php'; ?>
+</body>
+<!-- InstanceEnd --></html>
