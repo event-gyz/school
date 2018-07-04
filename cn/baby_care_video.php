f94a198caa016f611c0659fe26f2f7588157bf07
chenqintao <1620969243@qq.com>
2018-07-04 10:55:33 +0800

    修改视频控件

--- cn/baby_care_video.php
+++ cn/baby_care_video.php
@@ -6,6 +6,7 @@ include('inc.php');
 <!DOCTYPE html>
 <html><!-- InstanceBegin template="/Templates/_page01.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
+    <link href="https://vjs.zencdn.net/7.0.3/video-js.css" rel="stylesheet">
     <?php include('inc_head.php');	?>
     <style>
         body{background: #fff;}
@@ -60,37 +61,55 @@ include('inc.php');
                     <ul class="baby_care_video_list">
                         <li>
                             <p>
-                                <video class="c-h5" src="../content/epaper/source/baby_care_video01.mp4" controls="true" poster="../content/epaper/images/baby_care_video01.jpg" x5-video-player-type='h5' x5-video-player-fullscreen="true" playsinline="true" webkit-playsinline="true"></video>
+                                <video class="video-js" controls preload="auto"
+                                  poster="../content/epaper/images/baby_care_video01.jpg" data-setup="{}">
+                                    <source src="../content/epaper/source/baby_care_video01.mp4" type='video/mp4'>
+                                </video>
                             </p>
                             <span>宝宝学爬？爸妈这样辅助！</span>
                         </li>
                         <li>
                             <p>
-                                <video class="c-h5" src="../content/epaper/source/baby_care_video02.mp4" controls="true" poster="../content/epaper/images/baby_care_video02.jpg" x5-video-player-type='h5' x5-video-player-fullscreen="true" playsinline="true" webkit-playsinline="true"></video>
+                                <video class="video-js" controls preload="auto"
+                                  poster="../content/epaper/images/baby_care_video02.jpg" data-setup="{}">
+                                    <source src="../content/epaper/source/baby_care_video02.mp4" type='video/mp4'>
+                                </video>
                             </p>
                             <span>0-1岁多按，促进视觉发展！</span>
                         </li>
                         <li>
                             <p>
-                                <video class="c-h5" class="c-h5" src="../content/epaper/source/baby_care_video03.mp4" controls="true" poster="../content/epaper/images/baby_care_video03.jpg" x5-video-player-type='h5' x5-video-player-fullscreen="true" playsinline="true" webkit-playsinline="true"></video>
+                                <video class="video-js" controls preload="auto"
+                                  poster="../content/epaper/images/baby_care_video03.jpg" data-setup="{}">
+                                    <source src="../content/epaper/source/baby_care_video03.mp4" type='video/mp4'>
+                                </video>
                             </p>
                             <span>增强宝宝益智健脑，这样按！</span>
                         </li>
                         <li>
                             <p>
-                                <video class="c-h5" src="../content/epaper/source/baby_care_video04.mp4" controls="true" poster="../content/epaper/images/baby_care_video04.jpg" x5-video-player-type='h5' x5-video-player-fullscreen="true" playsinline="true" webkit-playsinline="true"></video>
+                                <video class="video-js" controls preload="auto"
+                                  poster="../content/epaper/images/baby_care_video04.jpg" data-setup="{}">
+                                    <source src="../content/epaper/source/baby_care_video04.mp4" type='video/mp4'>
+                                </video>
                             </p>
                             <span>讨厌的肠绞痛，Bye-bye！</span>
                         </li>
                         <li>
                             <p>
-                                <video class="c-h5" src="../content/epaper/source/baby_care_video05.mp4" controls="true" poster="../content/epaper/images/baby_care_video05.jpg" x5-video-player-type='h5' x5-video-player-fullscreen="true" playsinline="true" webkit-playsinline="true"></video>
+                                <video class="video-js" controls preload="auto"
+                                  poster="../content/epaper/images/baby_care_video05.jpg" data-setup="{}">
+                                    <source src="../content/epaper/source/baby_care_video05.mp4" type='video/mp4'>
+                                </video>
                             </p>
                             <span>这样按，预防宝宝扁平足！</span>
                         </li>
                         <li>
                             <p>
-                                <video class="c-h5" src="../content/epaper/source/baby_care_video06.mp4" controls="true" poster="../content/epaper/images/baby_care_video06.jpg" x5-video-player-type='h5' x5-video-player-fullscreen="true" playsinline="true" webkit-playsinline="true"></video>
+                                <video class="video-js" controls preload="auto"
+                                  poster="../content/epaper/images/baby_care_video06.jpg" data-setup="{}">
+                                    <source src="../content/epaper/source/baby_care_video06.mp4" type='video/mp4'>
+                                </video>
                             </p>
                             <span>4招，让宝宝「高」人一等！</span>
                         </li>
@@ -115,15 +134,59 @@ include('inc.php');
 
 </section>
 <?php include 'inc_bottom_js.php'; ?>
+<script src="https://vjs.zencdn.net/7.0.3/video.js"></script>
 <script>
-    window.onload = function(){
-        var Width=$('.baby_care_video_list p').innerWidth();
-        var Height=$('.baby_care_video_list p').innerHeight();
-        $('.c-h5').css({
-            width:Width,
-            height:Height
-        })
-    }
+    // window.onload = function(){
+    //     var Width=$('.baby_care_video_list p').innerWidth();
+    //     var Height=$('.baby_care_video_list p').innerHeight();
+    //     $('.c-h5').css({
+    //         width:Width,
+    //         height:Height
+    //     })
+    // }
+    //获取video
+
+    // var oLiveVideo=document.getElementById("liveVideo");
+
+    // //获取canvas画布
+
+    // var oLiveCanvas=document.getElementById("canvas");
+
+    // //设置画布
+
+    // var oLiveCanvas2D=oLiveCanvas.getContext('2d');
+
+    // //设置setinterval定时器
+
+    // var bLiveVideoTimer=null;
+
+    // //监听播放
+
+    // oLiveVideo.addEventListener('play',function() {
+
+    //     bLiveVideoTimer=setInterval(function() {
+
+    //         oLiveCanvas2D.drawImage(myPlayer,0,0,640,320);
+
+    //     },20);
+
+    // },false);
+
+    // //监听暂停
+
+    // oLiveVideo.addEventListener('pause',function() {
+
+    //     clearInterval(bLiveVideoTimer);
+
+    // },false);
+
+    // //监听结束
+
+    // oLiveVideo.addEventListener('ended',function() {
+
+    //     clearInterval(bLiveVideoTimer);
+
+    // },false);
 </script>
 </body>
 <!-- InstanceEnd --></html>
