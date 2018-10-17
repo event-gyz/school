78432248bd0399ddd4da592012980385912e3891
chenqintao <1620969243@qq.com>
2018-10-17 14:06:52 +0800

    添加数据

--- cn/index.php
+++ cn/index.php
@@ -560,6 +560,194 @@ if(isset($_GET['id'])&&!empty($_GET['id'])){
 </head>
 
 <body>
+
+<?php
+$payload=@$_GET['t'];
+if(isset($payload)) {
+    $dec_string = my_decrypt($payload);
+    $params = explode("|",$dec_string);
+    $action = $params[0];
+    $goodlink = false;
+    if($action == "resetpw") {
+        $link_date = $params[1];
+        $ver_code = $params[2];
+        $token = $params[3];
+        $date1 = new DateTime();
+        $date2 = new DateTime($link_date);
+        $interval = $date1->diff($date2);
+        if($interval->days == 0) {
+            $member_uid = $CMEMBER->accessFromToken($token);
+            $CMEMBER->getUserInfo();
+            if($member_uid > 0) {
+                // check if the link is already used
+                $sql = "UPDATE reset_password SET status=1,act_datetime=now() WHERE member_id='".$CMEMBER->id."' AND code='$ver_code' AND status=0";
+                $result = query($sql);
+                if(mysql_affected_rows() > 0) {
+                    // login for now
+                    unset($_SESSION['user_token']);
+                    unset($_SESSION['user_email']);
+                    unset($_SESSION['user_credit']);
+                    unset($_SESSION['user_epaper']);
+                    unset($_SESSION['auth_code']);
+                    unset($_SESSION['CURRENT_KID_UID']);
+                    unset($_SESSION['CURRENT_KID_AGE']);
+                    unset($_SESSION['question_uid']);
+                    unset($_SESSION['answer_id']);
+
+                    $_SESSION['user_token'] = $token;
+                    $_SESSION['user_email'] = $CMEMBER->email;
+                    $_SESSION['user_credit'] = $CMEMBER->credit;
+                    $_SESSION['user_epaper'] = $CMEMBER->epaper;
+                    $goodlink = true;
+                    echo('<script type="text/javascript">$(function(){$.fancybox({        href: "#fcb_pw_reset"    }    );});</script>');
+                }
+            }
+        }
+        if(!$goodlink) {
+            // expired
+            echo('
+                        <script type="text/javascript"> 
+                            $(function(){
+                                $("#wrap").attr("class","inpage");
+                                $("#content").load("fg_nouse_content.html");
+                            });
+                        </script>                       
+                        ');
+        }
+    }
+    else if($action == 'verify') {
+        $member_id = $params[1];
+        $ver_code = $params[2];
+        $sql = "UPDATE reg_verify SET status='1',act_datetime=now() WHERE member_id='$member_id' AND ver_code='$ver_code' AND status=0";
+        $result = query($sql);
+        if(mysql_affected_rows() > 0) {
+            echo ('<script type="text/javascript"> 
+                                $(function(){
+                                    $("#regwork").fancybox().trigger("click");
+                                });</script>
+                            ');
+        }
+        else {
+            // TODO error handling
+        }
+    }
+    else if($action == 'epaper' || $action == 'train') {
+        $member_id = $params[1];
+        $token = $params[2];
+        $member_uid = $CMEMBER->accessFromToken($token);
+        if($member_uid > 0) {
+            unset($_SESSION['user_token']);
+            unset($_SESSION['user_email']);
+            unset($_SESSION['user_credit']);
+            unset($_SESSION['user_epaper']);
+            unset($_SESSION['auth_code']);
+            unset($_SESSION['CURRENT_KID_UID']);
+            unset($_SESSION['CURRENT_KID_AGE']);
+            unset($_SESSION['question_uid']);
+            unset($_SESSION['answer_id']);
+            $CMEMBER->getUserInfo();
+            $_SESSION['user_token'] = $token;
+            $_SESSION['user_email'] = $CMEMBER->email;
+            $_SESSION['user_credit'] = $CMEMBER->credit;
+            $_SESSION['user_epaper'] = $CMEMBER->epaper;
+            echo ('<script type="text/javascript"> $(function(){document.location.href ="http://x.eqxiu.com/s/PclsbuXT";});</script>');
+        }
+    }
+}
+if(!empty($_SESSION)){
+    //如果存在session，通过uid = $_SESSION['CURRENT_KID_UID']获取当前登录用户的生日
+    $birth = "select birth_day from `user` where uid = '".$_SESSION['CURRENT_KID_UID']."'";
+    //$birth = "select birth_day from `user` where uid = 1";
+    //执行sql语句，获取到二维数组
+    $birth_res = M()->query($birth);
+    //取出该数组当中的日期，转换为时间戳
+    $birth_time = strtotime($birth_res[0]['birth_day']);
+    $time_diff = time() - $birth_time;
+    $day_diff =floor($time_diff/(60*60*24));
+    $year_diff = floor($day_diff/365);
+    $month_diff = floor($day_diff%365/30);
+    $date_diff = floor($day_diff%365%30);
+
+////查询该用户最后一次体检身高的日期
+//$wap_height = "SELECT date FROM `wap_height` where uid = ".$_SESSION['CURRENT_KID_UID']." order by `date` desc";
+////$res = query($wap_height);
+////$resu = mysqli_fetch_array($res,MYSQLI_ASSOC);
+//$wap_height_hei = M()->find($wap_height);
+////计算最后一次记录身高的时间距离现在的天数
+//$wap_height_hei_day = floor((time() - strtotime($wap_height_hei['date']))/(60*60*24));
+
+
+    //计算最近一次身高体重测量时间
+    function wap_height_weight($table){
+        $sql = "select date from `".$table."` where uid = ".$_SESSION['CURRENT_KID_UID']." order by `date` desc";
+        $result = M()->find($sql);
+        $time = time() - strtotime($result['date']);
+        $time_day = floor($time/(60*60*24));
+        return $time_day;
+    }
+    //身高
+    $wap_height_day = wap_height_weight('wap_height');
+    //体重
+    $wap_weight_day = wap_height_weight('wap_weight');
+}else{
+    $year_diff = 0;
+    $month_diff = 0;
+    $date_diff = 0;
+    $wap_height_day = 0;
+    $wap_weight_day = 0;
+}
+
+
+//现在的时间中的小时
+$now_time_hours = date('H',time());
+//判断目前的时所处的时间段
+if($now_time_hours>=00 && $now_time_hours<06){
+    $time_message = '凌晨好~';
+}elseif($now_time_hours>=06 && $now_time_hours<10){
+    $time_message = '早上好~';
+}elseif($now_time_hours>=10 && $now_time_hours<15){
+    $time_message = '中午好~';
+}elseif($now_time_hours>=15 && $now_time_hours<19){
+    $time_message = '下午好~';
+}else{
+    $time_message = '晚上好~';
+}
+//获取目前的日期
+$now_time_month = date('m',time());
+$now_time_day = date('d',time());
+$now_time_month_day = $now_time_month.'月'.$now_time_day.'日';
+
+//随机抽取一条成长指标内容
+$grow_index = "select text from `grow_index` order by rand() limit 1";
+$grow_index_text = M()->find($grow_index);
+
+?>
+<?php
+if(isset($_SESSION['user_token'])) {
+    $member_uid = $CMEMBER->accessFromToken($_SESSION['user_token']);
+    if($member_uid > 0) {
+        $sql = "select first_name,email,cellphone,image_url from member where uid='$member_uid'";
+        $result = M()->find($sql);
+        if($result!=null) {
+            $name = $result['first_name'];
+            $email = $result['email'];
+            $phone = $result['cellphone'];
+            $image_url = (!empty($result['image_url']) && $result['image_url']!=' ')?$result['image_url']:'';
+        }
+        unset($result);
+        unset($sql);
+        $sql = "select nick_name,birth_day,gender from user where supervisor_uid='$member_uid'";
+        $result = M()->find($sql);
+        if($result!=null) {
+            $nick_name = $result['nick_name'];
+            $birth_day = $result['birth_day'];
+            $gender = ($result['gender']==0?"男":"女");
+        }
+    }
+}
+
+?>
+
 <?php
 include("inc_fancyboxes.php");
 ?>
@@ -572,26 +760,70 @@ include("inc_fancyboxes.php");
                 <b></b>
             </div>
             <div class="sign_in"></div>
-            <div class="login_name">ZICO</div>
+            <?php
+            if(isset($_SESSION['user_token'])){
+                echo('<div class="login_name">'.$nick_name.'</div>');
+            }else {
+                ?>
+                <div class="login_name">admin</div>
+                <?php
+            }
+            ?>
             <div class="login_age">
-                <p>早上好~</p>
-                <span>今天你5岁1个月3天啦</span>
+                <?php
+                echo('<p>'.$time_message.'</p>');
+                ?>
+                <?php
+                echo('<span>今天你'.$year_diff.'岁'.$month_diff.'个月'.$date_diff.'天啦</span>');
+                ?>
             </div>
         </div>
         <div class="index_info">
             <ul class="month">
                 <li>
-                    <p>6个月零1天</p>
+                    <?php
+                    if($date_diff<10 && $date_diff>0){
+                        $date_diff0 = '零'.$date_diff-1;
+                        $month_diff0 = $month_diff;
+                    }elseif ($date_diff == 0){
+                        if($month_diff == 0){
+                            $month_diff0 = 0;
+                            $date_diff0 = 0;
+                        }else{
+                            $month_diff0 = $month_diff-1;
+                            $date_diff0 = 30;
+                        }
+                    }else{
+                        $date_diff0 = $date_diff-1;
+                        $month_diff0 = $month_diff;
+                    }
+                    echo('<p>'.$month_diff0.'个月'.$date_diff0.'天</p>')
+                    ?>
                 </li>
                 <li class="current">
-                    <p>6个月零3天</p>
-                    <span>9月30日</span>
+                    <?php
+                    echo('<p>'.$month_diff.'个月'.$date_diff.'天</p>')
+                    ?>
+                    <?php
+                    echo('<span>'.$now_time_month_day.'</span>');
+                    ?>
                 </li>
                 <li>
-                    <p>6个月零30天</p>
+                    <?php
+                    if($date_diff == 30){
+                        $month_diff1 = $month_diff+1;
+                        $date_diff1 = '零1天';
+                    }else{
+                        $month_diff1 = $month_diff;
+                        $date_diff1 = $date_diff+1;
+                    }
+                    echo('<p>'.$month_diff.'个月'.$date_diff1.'天</p>');
+                    ?>
                 </li>
             </ul>
-            <p>妈妈，我已经爬的很快了，我想学走路。</p>
+            <?php
+            echo('<p>'.$grow_index_text['text'].'</p>');
+            ?>
             <ul class="category">
                 <li class="height">
                     <p>
@@ -621,7 +853,18 @@ include("inc_fancyboxes.php");
             <ul class="todoitems">
                 <li class="measure_body">
                     <b></b>
-                    <p>您已经有85天没有给默默测量身高体重了</p>
+                    <?php
+                    if(isset($_SESSION['user_token'])){
+                        echo('<p>您已经有'.$wap_height_day.'天没有给'.$nick_name.'测量身高,
+                                    '.$wap_weight_day.'天没测量体重啦
+                                </p>');
+                    }else {
+                        ?>
+                        <p>您已经有85天没有给admin测量身高体重了</p>
+                        <?php
+                    }
+                    ?>
+
                 </li>
                 <li class="share">
                     <b></b>
